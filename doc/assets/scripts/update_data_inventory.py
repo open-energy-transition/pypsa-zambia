4426fcdf08d34f1adb8be31208d66e1258ef6ce8
ekatef <e.v.kasilova@gmail.com>
2026-05-11 17:06:05 +0200

    Add a script to update data inventory

---
+++ doc/assets/scripts/update_data_inventory.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+"""
+Extract metadata for the datasets used from `data/versions.csv` and 
+add them into `doc/data_inventory.csv` 
+
+How it runs:
+    - Automatically via the 'update-data-inventory' pre-commit hook
+    - Manually: python doc/assets/scripts/extract_config_snippets.py
+"""
+
+import re
+
+import pandas as pd
+
+from pathlib import Path
+
+def update_inventory(versions_csv="versions.csv", inventory_csv="data_inventory.csv"):
+    """
+    Extract meta-data on the latest datasets available to run the workflow
+    from `versions_csv` and place them into `inventory_csv`
+    """
+    priority_order = {"primary": 0, "build": 1, "archive": 2, "tutorial": 3}
+    category_type = pd.CategoricalDtype(categories=priority_order, ordered=True)
+
+    versions_df = (
+        pd.read_csv(versions_csv)
+        .assign(priority=lambda d: d["source"].str.lower().astype(category_type))
+        .sort_values(["dataset", "priority"])
+        .drop_duplicates("dataset", keep="first")[["dataset", "url"]]
+    )
+
+    print("versions_df")
+    print(versions_df)
+
+    inventory_df = pd.read_csv(inventory_csv)
+    missed_versions_df = versions_df.loc[~versions_df["dataset"].isin(inventory_df["Short name"]), ["dataset", "url"]]
+
+    inventory_df = pd.concat(
+        [inventory_df, missed_versions_df.rename(columns={"dataset": "Short name", "url": "Link to website"})],
+        ignore_index=True,
+    )
+
+    print("inventory_df")
+    print(inventory_df)
+
+    inventory_df.to_csv(inventory_csv, index=False)
+
+
+def main():
+    versions_file = Path("data", "versions.csv")
+    inventory_file = Path("doc", "data_inventory.csv")
+
+    update_inventory(
+        versions_csv=versions_file,
+        inventory_csv=inventory_file
+    )
+
+
+
+if __name__ == "__main__":
+    main()
